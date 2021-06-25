c867ea0cb8410d900fe600eaa55dc4b178c982a3
Michael Fox <mikefox17@gmail.com>
2021-06-25 12:42:18 -0400

    markdown is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './index.css';
 import React, { useState } from 'react';
+import marked from 'marked';
 
 function App() {
-    const [markdown, setMarkdown] = useState('sup');
+    const [markdown, setMarkdown] = useState('# sup');
+
+    function handleChange(e) {
+        setMarkdown(e.target.value);
+    }
+
     return (
         <div className='app'>
-            <div>Markdown Area</div>
-            <textarea />
+            <textarea onChange={handleChange} value={markdown} />
+            <div
+                className='preview'
+                dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+            />
         </div>
     );
 }
